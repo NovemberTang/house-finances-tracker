48355ef517c9dd8b0f7e829163afe344e9b1427b
NovemberTang <nthrale@protonmail.com>
2020-11-21 20:52:44 +0000

    rejigged tests

--- src/test/scala/json/JsonConverterSpec.scala
+++ src/test/scala/json/JsonConverterSpec.scala
@@ -6,43 +6,55 @@ import org.scalatest.matchers.should.Matchers
 
 class JsonConverterSpec extends AnyFlatSpec with Matchers with JsonConverters {
 
-  val alice = new Person("Bob", 678)
-  val bob = new Person("Alice", -9856.45)
+  private val bob = new Person("Bob", 678)
+  private val alice = new Person("Alice", -9856.45)
 
-  "Valid person JSON" should "convert correctly into a Person object" in {
-    val bobJson =
-      """{
-        |  "name" : "Bob",
-        |  "balance" : 678.0
-        |}""".stripMargin
+  private val bobJson =
+    """{
+      |  "name" : "Bob",
+      |  "balance" : 678.0
+      |}""".stripMargin
 
-    val aliceJson =
-      """{
-        |  "name" : "Alice",
-        |  "balance" : -9856.45
-        |}""".stripMargin
+  private val aliceJson =
+    """{
+      |  "name" : "Alice",
+      |  "balance" : -9856.45
+      |}""".stripMargin
 
+  private val aliceBobJson =
+    """[
+      |  {
+      |    "name" : "Alice",
+      |    "balance" : -9856.45
+      |  },
+      |  {
+      |    "name" : "Bob",
+      |    "balance" : 678.0
+      |  }
+      |]""".stripMargin
 
-    personToJson(alice) shouldEqual bobJson
-    personToJson(bob) shouldEqual aliceJson
-
+  "A Person object" should "convert correctly into a Person object" in {
+    personToJson(alice) shouldEqual aliceJson
+    personToJson(bob)   shouldEqual bobJson
   }
 
-  "A list of people" should "correctly convert into JSON" in{
-    val people = List(alice, bob)
-    val expectedResult: String ="""[
-                                   |  {
-                                   |    "name" : "Bob",
-                                   |    "balance" : 678.0
-                                   |  },
-                                   |  {
-                                   |    "name" : "Alice",
-                                   |    "balance" : -9856.45
-                                   |  }
-                                   |]""".stripMargin
+  "A list of Person objects" should "correctly convert into JSON" in {
+    peopleToJson(List(alice, bob)) shouldEqual aliceBobJson
+  }
 
-    peopleToJson(people) shouldEqual expectedResult
+  "A valid person json" should "correctly convert into a Person object" in {
+    val actualPerson: Person = jsonToPerson(aliceJson).getOrElse(throw new Exception)
+    actualPerson.balance shouldEqual alice.balance
+    actualPerson.name    shouldEqual alice.name
+  }
 
+  "A valid json containing more than one person" should "correctly convert into a List of Persons" in {
+    val personList: List[Person] = jsonToPeople(aliceBobJson).getOrElse(throw new Exception)
+    personList.head.balance shouldEqual alice.balance
+    personList.head.name    shouldEqual alice.name
+
+    personList.last.balance shouldEqual bob.balance
+    personList.last.name    shouldEqual bob.name
   }
 
 }
