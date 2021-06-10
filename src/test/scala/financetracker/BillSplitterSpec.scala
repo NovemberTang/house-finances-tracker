e51e201d9da796afdf6bc2ea253291eadfad487f
Natasha Thrale <nthrale@protonmail.com>
2021-06-10 14:02:41 +0100

    Add test for uneven splitting of the costs

--- src/test/scala/financetracker/BillSplitterSpec.scala
+++ src/test/scala/financetracker/BillSplitterSpec.scala
@@ -20,4 +20,12 @@ class BillSplitterSpec extends AnyFlatSpec with Matchers{
     actualResult.map(_.toString) shouldEqual List("Alice is owed £33.33", "Bob owes £16.67", "Cat owes £16.67")
   }
 
+  "An uneven split" should "work correctly" in {
+    val quarter = new Fraction(0.25)
+    val half = new Fraction(0.5)
+    val actualResult = splitTheMoney("Alice", 100, List(alice, bob, cat), Some(List(half, quarter, quarter)))
+    actualResult.map(_.toString) shouldEqual List("Alice is owed £0.50", "Bob owes £0.25", "Cat owes £0.25")
+
+  }
+
 }
